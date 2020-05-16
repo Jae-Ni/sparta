8a358153c1c9368829e2847a14ad5d955e17de19
Jae-Ni <61180441+Jae-Ni@users.noreply.github.com>
2020-05-16 18:45:49 +0900

    asd

--- homework/1/index.js
+++ homework/1/index.js
@@ -1,7 +1,24 @@
 const btn = document.querySelector(".btn");
+let name = document.getElementById("name");
+let cnt = document.getElementById("cnt");
+let number = document.getElementById("number");
+let adr = document.getElementById("address");
 
 function handleClick(event) {
-  alert("주문이 완료되었습니다. 조심하세요!");
+  if (
+    name.value == "" ||
+    cnt.value == "0" ||
+    number.value == "" ||
+    adr.value == ""
+  ) {
+    alert("빠짐없이 입력해 주세요");
+  } else {
+    alert("주문이 완료되었습니다. 조심하세요!");
+    console.log(name.value);
+    console.log(cnt.value);
+    console.log(adr.value);
+    console.log(number.value);
+  }
 }
 
 function init() {
